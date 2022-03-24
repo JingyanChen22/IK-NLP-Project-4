28e24b87ff97f4a86e3a0fe2335f07211fafb3a3
Arjan van Eerden <Arjanvaneerden@gmail.com>
2022-03-24 23:59:26 +0100

    removed some error

--- dutch/createDatasets.py
+++ dutch/createDatasets.py
@@ -1,7 +1,10 @@
 from makeWordListDutch import createDatasets
 
 orthoList = createDatasets('ortho')
-print(orthoList[:5]) # print 5 examples
+print(orthoList[:6]) # print 5 examples
 
 pronList = createDatasets('pron')
-print(pronList[:5]) # print 5 examples
+print(pronList[:6]) # print 5 examples
+
+print("\nThere are", len(orthoList), "orthographical wordpairs.")
+print("\nThere are", len(pronList), "phonetical wordpairs.")
