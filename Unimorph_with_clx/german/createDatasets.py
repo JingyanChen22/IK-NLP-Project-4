cd548061e9e4dd51cbf37a65b4027ab6a25de217
Arjan van Eerden <Arjanvaneerden@gmail.com>
2022-03-28 01:25:20 +0200

    made nice folder

---
+++ Unimorph_with_clx/german/createDatasets.py
@@ -0,0 +1,11 @@
+from makePythonDictionairies import createDatasets
+
+orthoList = createDatasets('ortho')
+print("\nThere are", len(orthoList), "orthographical wordpairs:")
+print("   PRESENT:     PAST:    REGULAR:")
+print(orthoList[:6], "and so on...") # print 5 examples
+
+pronList = createDatasets('pron')
+print("\nThere are", len(pronList), "phonetical wordpairs:")
+print("   PRESENT:       PAST:       REGULAR:")
+print(pronList[:6], "and so on...") # print 5 examples
